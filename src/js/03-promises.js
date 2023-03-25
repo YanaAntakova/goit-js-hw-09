5e718464fac0dc00c2203fc28a9e3f9d1197eb9b
YanaAntakova <114938428+YanaAntakova@users.noreply.github.com>
2023-03-25 10:40:31 +0200

    .

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,53 +1,40 @@
 import Notiflix from 'notiflix';
 
-const form = document.querySelector('form.form');
-const options = {
-  position: 'center-bottom',
-  distance: '15px',
-  borderRadius: '15px',
-  timeout: 10000,
-  clickToClose: true,
-  cssAnimationStyle: 'from-right',
-};
+const form = document.querySelector('.form');
+const delayValue = document.querySelector('input[name=delay]');
+const stepValue = document.querySelector('input[name=step]');
+const amountValue = document.querySelector('input[name=amount]');
+form.addEventListener('submit', handleFormSubmit);
 
-form.addEventListener('click', onPromiseCreate);
+function handleFormSubmit(e) {
+  e.preventDefault();
+  let delay = parseInt(delayValue.value);
+  const step = parseInt(stepValue.value);
+  const amount = parseInt(amountValue.value);
+  if (delay < 0 || step < 0 || amount <= 0) {
+    Notiflix.Report.failure(
+      'Incorrect value',
+      'Enter correct values',
+      'Continue'
+    );
+    return;
+  }
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(result => Notiflix.Notify.success(result))
+      .catch(error => Notiflix.Notify.failure(error));
+    delay += step;
+  }
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
       }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
     }, delay);
   });
-}
-
-function onPromiseCreate(e) {
-  e.preventDefault();
-  const { delay, step, amount } = e.currentTarget.elements;
-  let inputDelay = Number(delay.value);
-  let inputStep = Number(step.value);
-  let inputAmount = Number(amount.value);
-
-  for (let i = 1; i <= inputAmount; i += 1) {
-    inputDelay += inputStep;
-
-    createPromise(i, inputDelay)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`,
-          options
-        );
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`,
-          options
-        );
-      });
-    e.currentTarget.reset();
-  }
 }
